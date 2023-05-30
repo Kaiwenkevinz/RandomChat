c815e06e77e9a4633c49560311b5e9cdd292d2eb
Kaiwen Zhang <kevin.zhangkw@gmail.com>
2023-05-30 12:22:40 +0800

    add test for custom hook

--- src/__tests__/ChatWebSocket.test.tsx
+++ src/__tests__/ChatWebSocket.test.tsx
@@ -1,15 +1,16 @@
 // import renderer from 'react-test-renderer';
-import {renderHook} from '@testing-library/react';
+// import {renderHook, act} from '@testing-library/react';
+import {renderHook, act, waitFor} from '@testing-library/react-native';
 
 import WS from 'jest-websocket-mock';
 import {WEB_SOCKET_URL} from '../constant';
-import {useChatWebSocket} from '../hooks/useChatWebsocket';
+import {useChatWebSocket} from '../hooks/useChatWebSocket';
 
 jest.mock('@react-navigation/native', () => ({
   useNavigation: () => ({navigate: jest.fn()}),
 }));
 
-it('the server keeps track of received messages, and yields them as they come in', async () => {
+it('The mocked WebSocket server should be running', async () => {
   const server = new WS('ws://localhost:1234');
   const client = new WebSocket('ws://localhost:1234');
 
@@ -19,16 +20,15 @@ it('the server keeps track of received messages, and yields them as they come in
   expect(server).toHaveReceivedMessages(['hello']);
 });
 
-/**
- * @jest-environment jsdom
- */
-test('useWebSocket hook', async () => {
-  const server = new WS(WEB_SOCKET_URL);
+describe('useWebSocket hook', () => {
+  it('Once onOpen, client sends msg to server.', async () => {
+    const server = new WS(WEB_SOCKET_URL);
 
-  renderHook(() => useChatWebSocket());
+    renderHook(() => useChatWebSocket());
 
-  await server.connected;
+    await server.connected;
 
-  await expect(server).toReceiveMessage('WebSocket Client Connected');
-  expect(server).toHaveReceivedMessages(['WebSocket Client Connected']);
+    await expect(server).toReceiveMessage('WebSocket Client Connected');
+    expect(server).toHaveReceivedMessages(['WebSocket Client Connected']);
+  });
 });
